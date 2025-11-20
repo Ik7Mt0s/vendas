16b75ce19f1da40507f11ce4ebc5cd881f2d8aa2
Ik7Mt0s <icaromatoscb@gmail.com>
2025-11-20 15:42:54 -0300

    correção Estoque

--- src/main/java/br/icev/vendas/Estoque.java
+++ src/main/java/br/icev/vendas/Estoque.java
@@ -2,18 +2,35 @@ package br.icev.vendas;
 
 import br.icev.vendas.excecoes.QuantidadeInvalidaException;
 import br.icev.vendas.excecoes.SemEstoqueException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Estoque {
+    private final Map<String, Integer> estoque = new HashMap<>();
+
     public void adicionarEstoque(String codigo, int quantidade) throws QuantidadeInvalidaException {
-        throw new UnsupportedOperationException("TODO");
+        if (quantidade <= 0) {
+            throw new QuantidadeInvalidaException("Quantidade deve ser maior que zero");
+        }
+        
+        estoque.put(codigo, estoque.getOrDefault(codigo, 0) + quantidade);
     }
 
     public int getDisponivel(String codigo) {
-        throw new UnsupportedOperationException("TODO");
+        return estoque.getOrDefault(codigo, 0);
     }
 
     public void reservar(String codigo, int quantidade)
             throws SemEstoqueException, QuantidadeInvalidaException {
-        throw new UnsupportedOperationException("TODO");
+        if (quantidade <= 0) {
+            throw new QuantidadeInvalidaException("Quantidade deve ser maior que zero");
+        }
+        
+        int disponivel = getDisponivel(codigo);
+        if (disponivel < quantidade) {
+            throw new SemEstoqueException("Estoque insuficiente para " + codigo);
+        }
+        
+        estoque.put(codigo, disponivel - quantidade);
     }
 }
