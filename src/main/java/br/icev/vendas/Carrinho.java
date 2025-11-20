9b9ab6a7bf3a1f62a6da89cb913afaa67c0d38e2
Ik7Mt0s <icaromatoscb@gmail.com>
2025-11-20 14:31:23 -0300

    correção Carrinho

--- src/main/java/br/icev/vendas/Carrinho.java
+++ src/main/java/br/icev/vendas/Carrinho.java
@@ -2,21 +2,56 @@ package br.icev.vendas;
 
 import br.icev.vendas.excecoes.QuantidadeInvalidaException;
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Carrinho {
+    private final Map<String, Produto> produtos = new HashMap<>();
+    private final Map<String, Integer> quantidades = new HashMap<>();
+
     public void adicionar(Produto produto, int quantidade) throws QuantidadeInvalidaException {
-        throw new UnsupportedOperationException("TODO");
+        if (quantidade <= 0) {
+            throw new QuantidadeInvalidaException("Quantidade deve ser  maior que zero");
+        }
+
+        String codigo = produto.getCodigo();
+        produtos.put(codigo, produto);
+
+        int quantidadeAtual = quantidades.getOrDefault(codigo, 0);
+        quantidades.put(codigo, quantidadeAtual + quantidade);
     }
 
     public BigDecimal getSubtotal() {
-        throw new UnsupportedOperationException("TODO");
+        BigDecimal subtotal = BigDecimal.ZERO;
+
+        for (String codigo : quantidades.keySet()) {
+
+            Produto produto = produtos.get(codigo);
+            int quantidade = quantidades.get(codigo);
+            BigDecimal precoItem = produto.getPrecoUnitario().multiply(new BigDecimal(quantidade));
+
+            subtotal = subtotal.add(precoItem);
+        }
+
+        return UtilDinheiro.arredondar2(subtotal);
     }
 
     public BigDecimal getTotalCom(PoliticaDesconto politica) {
-        throw new UnsupportedOperationException("TODO");
+        BigDecimal subtotal = getSubtotal();
+        BigDecimal totalComDesconto = politica.aplicar(subtotal);
+        
+        if (totalComDesconto.compareTo(BigDecimal.ZERO) < 0) {
+            return BigDecimal.ZERO;
+        }
+        
+        return UtilDinheiro.arredondar2(totalComDesconto);
     }
 
     public int getTotalItens() {
-        throw new UnsupportedOperationException("TODO");
+        int total = 0;
+        for (int quantidade : quantidades.values()) {
+            total += quantidade;
+        }
+        return total;
     }
 }
